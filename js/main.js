eafd606f46b144167a3bd92fa2e7207771f9d0b9
Vladislav <eclipsik69@gmail.com>
2018-07-04 13:43:59 +0300

    add new pages

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.sl-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    appendArrows: '.sl-slider-control',
+    prevArrow: '<div class="sl-prev"></div>',
+    nextArrow: '<div class="sl-next"></div>'
+});
+
