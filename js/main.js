0a860e673de6effffb326fc531b66b1edb21367d
Vladislav <eclipsik69@gmail.com>
2018-07-17 17:32:30 +0300

    add prod,categories adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+$('.chat-toggle').click(function () {
+    $('.chat').fadeIn(500)
+})
+
+$('.chat-close').click(function () {
+    $('.chat').fadeOut(500)
+})
+
+$(function () {
+    var doc = $(document).height();
+    console.log(doc)
+    $(window).scroll(function () {
+        if($(window).scrollTop() > doc/4) {
+            $('.chat-toggle').fadeIn(500);
+        }
+    })
+})
+
 $('.sl-slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -6,4 +24,3 @@ $('.sl-slider').slick({
     prevArrow: '<div class="sl-prev"></div>',
     nextArrow: '<div class="sl-next"></div>'
 });
-
